881a4127ba185d1438701448256d697072ee1715
Computer  6 <you@example.com>
2023-02-02 16:35:57 -0500

    amorgus

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,17 +8,21 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
-
+  TalonFX motor = new TalonFX(1);
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -28,6 +32,13 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    motor.configFactoryDefault();
+        motor.setInverted(false);
+        motor.setNeutralMode(NeutralMode.Brake);
+        motor.configPeakOutputForward(1);
+        motor.configPeakOutputReverse(-1);
+        motor.configVoltageCompSaturation(10);
+        motor.enableVoltageCompensation(true);
   }
 
   /**
@@ -81,7 +92,10 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    
+    motor.set(ControlMode.PercentOutput,10);
+  }
 
   @Override
   public void testInit() {
